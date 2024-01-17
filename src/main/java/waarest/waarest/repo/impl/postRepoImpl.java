6d2a6cd7f603ba1aa2ac76c1acc942b6e92431f1
vijay9711 <vijaysm97@gmail.com>
2024-01-17 14:32:56 -0600

    lab 1 more data added

--- src/main/java/waarest/waarest/repo/impl/postRepoImpl.java
+++ src/main/java/waarest/waarest/repo/impl/postRepoImpl.java
@@ -14,8 +14,14 @@ public class postRepoImpl implements PostRepo{
   private static List<Post> postList;
   static{
     postList = new ArrayList<Post>();
-    Post p1 = new Post(100,"vijay", "content", "event");
+    Post p1 = new Post(100,"Social", "content about social", "James");
+    Post p2 = new Post(101,"Event", "Node js with mongoDB event", "Lil ni");
+    Post p3 = new Post(102,"Friendly", "Feel to share friendly post.", "Ana kim");
+    Post p4 = new Post(103,"Snowboard", "Outdoor activity in winter.", "Tim nick");
     postList.add(p1);
+    postList.add(p2);
+    postList.add(p3);
+    postList.add(p4);
   }
 
 
